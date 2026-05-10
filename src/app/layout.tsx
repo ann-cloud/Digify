06df249335884c9b4141e58b48b9b3999c326637
annnychkka <aniutka.lviv@gmail.com>
2026-05-10 22:40:25 +0300

    Add Clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { getCurrentUser } from '@/lib/auth';
 import { Header } from '@/components/Header';
@@ -39,6 +40,13 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <main>{children}</main>
         <Footer />
         <GoogleAnalytics gaId="G-4S0QD92NXR" />
+        <Script id="ms-clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "wp2pj68ewz");`}
+        </Script>
       </body>
     </html>
   );
