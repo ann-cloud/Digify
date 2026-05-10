c657a5db107f51414947b288d2e3145a18a998ca
annnychkka <aniutka.lviv@gmail.com>
2026-05-10 23:11:05 +0300

    Add Plausible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,6 +48,17 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
           })(window, document, "clarity", "script", "wp2pj68ewz");`}
         </Script>
+          <Script
+            src="https://plausible.io/js/pa-UIODoaVapPpYeaOoIV6NU.js"
+            strategy="afterInteractive"
+          />
+          <Script id="plausible-init" strategy="afterInteractive">
+            {`
+              window.plausible = window.plausible || function() { (plausible.q = plausible.q || []).push(arguments) };
+              plausible.init = plausible.init || function(i) { plausible.o = i || {} };
+              plausible.init();
+            `}
+          </Script>
         <Analytics />
       </body>
     </html>
